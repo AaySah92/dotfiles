c293319e4c65c33db209b0217c1472c3fc69155a
Aayush Sahay <aayush.sahay1992@gmail.com>
2024-05-01 23:10:52 +0100

    Added Neotree and some wallpapers

--- .config/nvim/lua/asahay/lazy.lua
+++ .config/nvim/lua/asahay/lazy.lua
@@ -37,8 +37,18 @@ require("lazy").setup({
 	{'numToStr/Comment.nvim'},
 	{'christoomey/vim-tmux-navigator'},
 	{
-	  'stevearc/oil.nvim',
-	  opts = {},
-	  dependencies = { "nvim-tree/nvim-web-devicons" },
+		'stevearc/oil.nvim',
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		}
 	},
 })
